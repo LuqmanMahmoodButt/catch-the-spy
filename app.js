36ba8001c834da44dfb6aaa06e5d778b805e22db
Luqman B <luqmanmbutt.dev@gmail.com>
2024-05-07 11:40:43 +0100

    click event is working when you click the nose it also added a point into score

--- app.js
+++ app.js
@@ -1,18 +1,62 @@
-const score = document.querySelector('#score')
+const score = document.querySelector('.score')
 const holes = document.querySelectorAll('.hole')
 const nose = document.querySelector('.nose')
 
 
 
 let result = 0
+let hitNose
+let gameTime = 60
 
+console.log(result);
+
+// console.log(hitNose)
 
 const randomHole = () => {
     holes.forEach(hole => {
-       hole.classList.remove('nose') 
+        hole.classList.remove('nose')
     })
 
     let randomNose = holes[Math.floor(Math.random() * 9)]
-        randomNose.classList.add('nose')
+    randomNose.classList.add('nose')
+
+    hitNose = randomNose.id
+
+    console.log(randomNose.id);
 }
-randomHole()
+
+// const handleClick = (event) => {
+//     holes.forEach(hole => {
+//         hole.addEventListener('click', handleClick)
+//     })
+// }
+
+holes.forEach(hole => {
+    hole.addEventListener('click', () => {
+        if (hole.id === hitNose) {
+            result++
+            score.textContent = result
+            hitNose = null
+            console.log('i have clicked the nose');
+        }
+        // console.log(hitNose)
+    })
+})
+
+
+
+
+const moveNose = () => {
+    setInterval(randomHole, 500)
+}
+// randomHole(moveNose())
+
+
+
+
+
+
+
+
+
+
