c8a8c773e0511c99e1f933259ddfabc161e7e611
Luqman B <luqmanmbutt.dev@gmail.com>
2024-05-09 00:49:52 +0100

    finished the logic for difficulty buttons created a start button that resets the game if you click it at the end opf timer

--- app.js
+++ app.js
@@ -1,84 +1,133 @@
 const score = document.querySelector('.score')
 const holes = document.querySelectorAll('.hole')
-const nose = document.querySelector('.nose')
+const spy = document.querySelector('.spy')
 const time = document.querySelector(".gametime")
+const startButton = document.querySelector('.gamestart')
+const easy = document.querySelector('.easybutton')
+const medium = document.querySelector('.mediumbutton')
+const hard = document.querySelector('.hardbutton')
+const endResult = document.querySelector('.end-game-message')
+const cursor = document.querySelector(".cursor img")
 
+window.addEventListener("mousemove", (e) => {
+    cursor.style.top = e.pageY + "px"
+    cursor.style.left = e.pageX + "px"
 
+})
 
-let result = 0
-let hitNose
-let gameTime = 60
-let timer = null
-
-console.log(result);
-
-// console.log(hitNose)
-
-const randomHole = () => {
-    holes.forEach(hole => {
-        hole.classList.remove('nose')
-    })
-
-    let randomNose = holes[Math.floor(Math.random() * 9)]
-    randomNose.classList.add('nose')
-
-    hitNose = randomNose.id
-
-    console.log(randomNose.id);
-}
-
-// const handleClick = (event) => {
-//     holes.forEach(hole => {
-//         hole.addEventListener('click', handleClick)
-//     })
-// }
-
-holes.forEach(hole => {
-    hole.addEventListener('click', () => {
-        if (hole.id === hitNose) {
-            result++
-            score.textContent = `Your Score: ${result}`
-            hitNose = null
-            console.log('i have clicked the nose');
-        }
-        // console.log(hitNose)
-    })
+window.addEventListener("click", () => {
+    cursor.style.animation = "hit 0.1s ease"
+    setTimeout(() => {
+        cursor.style.removeProperty("animation")
+    }, 100);
 })
 
 
+let easySpeed = false
+let mediumSpeed = false
+let hardSpeed = false
+let gameSpeed = 1000 
+let result = 0
+let hitSpy
+let gameTime = 60
+let timer = null
+time.textContent = `Game Timer: ${gameTime}`
 
+easy.addEventListener('click', () =>{
+    gameSpeed = 1000
+    easySpeed = true
 
-const moveNose = () => {
-    setInterval(randomHole, 1000)
+})
+
+medium.addEventListener('click', () =>{
+    gameSpeed = 500
+})
+
+hard.addEventListener('click', () =>{
+    gameSpeed = 300
+})
+
+const randomHole = () => {
+    holes.forEach(hole => {
+        hole.classList.remove('spy')
+    })
+
+    let randomSpy = holes[Math.floor(Math.random() * 9)]
+    randomSpy.classList.add('spy')
+
+    hitSpy = randomSpy.id
+
+    console.log(randomSpy.id);
 }
-// randomHole(moveNose())
 
-
-// gameCountDown = setInterval(countDown, 1000)
-
-
+holes.forEach(hole => {
+    hole.addEventListener('click', () => {
+        if (hole.id === hitSpy) {
+            result++
+            score.textContent = `Your Score: ${result}`
+            hitSpy = null
+            console.log('i have clicked the spy');
+        }
+    })
+})
 
 const countDown = () => {
     gameTime--
     time.textContent = `Game Timer: ${gameTime}`
-
-    if (gameTime == 0) {
-        clearInterval(countDownTimer)
-        clearInterval(time)
+    
+    if (gameTime === 0) {
+        // if (easySpeed && result >= 50) {
+        //     endResult.textContent = 'you are winner winner chicken dinner'
+        // } else {
+        //     endResult.textContent = 'you are loser'
+        // }
+        // if (mediumSpeed && result >= 10) {
+        //     endResult.textContent = 'you are winner winner chicken dinner'
+        // } else {
+        //     endResult.textContent = 'you are loser'
+        // }
+        // if (hardSpeed && result >= 10) {
+        //     endResult.textContent = 'you are winner winner chicken dinner'
+        // } else {
+        //     endResult.textContent = 'you are loser'
+        // }
+        // endResult.textContent = `Your High-score Is:${result}`
+        // this code resets the game
+        clearInterval(timer)
+        clearInterval(spyMovement)
+        timer = null
+        result = 0
     }
 }
+// let interval = setInterval(countDown, 1000)
 
-let countDownTimer = setInterval(countDown, 1000)
 
-//  function timer () {
-// let seconds = 1000
-//     for(gameTime = 60; gameTime <= 0; gameTime--){
-//         setTimeout(() => {
-//             seconds += 1000;
-//             time.textContent = gameTime
-//         }, seconds);
-        
-//     }
-// }
-    
-// window.onload = timer()
+const moveSpy = () => {
+    spyMovement = setInterval(randomHole, gameSpeed)
+}
+
+const startGameButton = () => {
+    if (timer) return; 
+
+    result = 0; 
+    score.textContent = `Your Score: ${result}`;
+
+    gameTime = 10; 
+    time.textContent = `Game Timer: ${gameTime}`;
+    timer = setInterval(countDown, 1000);
+
+    moveSpy();
+    countDown()
+}
+
+
+startButton.addEventListener('click', startGameButton);
+
+
+
+
+
+
+
+
+
